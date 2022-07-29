248d9b9981a7023eac2fe1392096e1df7edcee95
Suleyman Ismayilov <ismayilov.suleyman0304@gmail.com>
2022-07-29 12:56:32 +0400

    commit

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const aside = document.getElementById("aside");
 const main = document.getElementById("main");
-
+const nav_toggler = document.getElementById("nav_toggler")
 
 function toggleAside() {
     if( aside.style.width = "270px" &&  aside.style.left != "-270px"){
         aside.style.left = "-270px";
         aside.style.width = "0px";
-        main.style.paddingLeft = "0px";}    
+        main.style.paddingLeft = "0px"
+        nav_toggler.style.left = "30px";}    
     else{
         aside.style.left = "0px";
         aside.style.width = "270px";
-        main.style.paddingLeft = "270px";          
+        main.style.paddingLeft = "270px"; 
+        nav_toggler.style.left = "300px"         
     }
 }
